b6d871129131b4ef06cb79800f9afd1651da2e15
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2025-09-10 14:10:55 +0300

    Initial Commit

---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Vladislav Miroshnichenko on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
